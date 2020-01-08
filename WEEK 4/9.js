fbf985b403f05ee028702e0ddacbfe7ee17a321f
darindra23 <58590046+darindra23@users.noreply.github.com>
2020-01-08 14:00:38 +0700

    u

---
+++ WEEK 4/9.js
@@ -0,0 +1,21 @@
+function checkAB(num) {
+    // you can only write your code here!
+    var jarak = 4
+    var check = false
+
+    for (i = 0; i < num.length; i++) {
+        if(num[i] === 'a' && num[i+jarak] === 'b'){
+            check = true
+        } else if(num[i] === 'b' && num[i+jarak] === 'a') {
+            check = true
+        }
+    }
+    return check
+  }
+  
+  // TEST CASES
+  console.log(checkAB('lane borrowed')); // true
+  console.log(checkAB('i am sick')); // false
+  console.log(checkAB('you are boring')); // true
+  console.log(checkAB('barbarian')); // true
+  console.log(checkAB('bacon and meat')); // false
